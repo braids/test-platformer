19d8ae37238e7d615a856a4386ddd72701327f0a
braids <cymoro@gmail.com>
2018-02-13 13:28:39 -0800

    BSP Tree management
    - BSP_Tree.cpp
    -- Moved commented func to bottom of file.
    -- SDL_IntersectRect calls changed to SDL_HasIntersection.
    -- nullptr checks simplified.

--- test-platformer/Physics/BSP_Tree.cpp
+++ test-platformer/Physics/BSP_Tree.cpp
@@ -16,7 +16,7 @@ void BSP_Tree<Data>::AddItems(NodeVec _list) {
 template <typename Data>
 void BSP_Tree<Data>::FindItems(DataVec* items, SDL_Rect* _area) {
 	// If no subtrees, add current item list
-	if (this->front == nullptr && this->back == nullptr) {
+	if (!this->front && !this->back) {
 		// Add all node data to the data list
 		items.insert(items.end(), this->list.begin(), this->list.end());
 		
@@ -25,72 +25,20 @@ void BSP_Tree<Data>::FindItems(DataVec* items, SDL_Rect* _area) {
 	}
 
 	// If front subtree exists and collides, check subtree
-	if (this->front != nullptr) {
-		if (SDL_IntersectRect(this->front->partition, _area))
+	if (this->front) {
+		if (SDL_HasIntersection(this->front->partition, _area))
 			front->FindItems(items, _area);
 	}
 
 	// If front subtree exists and collides, check subtree
-	if (this->back != nullptr) {
-		if (SDL_IntersectRect(this->back->partition, _area)) {
+	if (this->back) {
+		if (SDL_HasIntersection(this->back->partition, _area)) {
 			back->FindItems(items, _area);
 		}
 	}
 	
 	return;
 }
-
-/*
-template <typename T>
-void BSP_Tree<T>::BuildTree(BSP_Tree* _tree, NodeVec _list, SDL_Rect* _partition, int branchLevel) {
-	// Set partition
-	_tree->partition = _partition;
-
-	// Add list to current tree
-	_tree->AddItems(_list);
-
-	// If no sub-branches, return.
-	if (branchLevel == 0)
-		return;
-
-	SDL_Rect* frontRect;
-	SDL_Rect* backRect;
-	NodeVec frontList;
-	NodeVec backList;
-
-
-	// Set front/back rects
-	if (_tree->partition->w > _tree->partition->h) {
-		Uint32 halfWidth = _tree->partition->w / 2;
-		frontRect = Graphics::CreateRect(halfWidth, _tree->partition->h, _tree->partition->x, _tree->partition->y);
-		backRect = Graphics::CreateRect(halfWidth, _tree->partition->h, _tree->partition->x + halfWidth, _tree->partition->y);
-	}
-	else {
-		Uint32 halfHeight = _tree->partition->h / 2;
-		frontRect = Graphics::CreateRect(_tree->partition->w, halfHeight, _tree->partition->x, _tree->partition->y);
-		backRect = Graphics::CreateRect(_tree->partition->w, halfHeight, _tree->partition->x, _tree->partition->y + halfHeight);
-	}
-
-
-	// Sort items into partition lists
-	for (T* item : _list) {
-		if (SDL_HasIntersection(item->Rect(), frontRect))
-			frontList.push_back(item);
-		if (SDL_HasIntersection(item->Rect(), backRect))
-			backList.push_back(item);
-	}
-
-	// If a list has items, create new sub-branch.
-	if (!frontList.empty()) {
-		_tree->front = new BSP_Tree();
-		BSP_Tree::BuildTree(_tree->front, frontList, frontRect, branchLevel - 1);
-	}
-	if (!backList.empty()) {
-		_tree->back = new BSP_Tree();
-		BSP_Tree::BuildTree(_tree->back, backList, backRect, branchLevel - 1);
-	}
-}
-*/
 
 template<typename Data>
 void BSP_Tree<Data>::BuildTree(BSP_Tree* _tree, NodeVec _list, SDL_Rect* _area) {
@@ -144,3 +92,57 @@ void BSP_Tree<Data>::BuildTree(BSP_Tree* _tree, NodeVec _list, SDL_Rect* _area) 
 	}
 }
 
+
+// Below func uses branchlevel to determine how deep the tree goes. I don't want to delete it 
+// just yet, the code can be appropriated later for neat BSP tree funcs.
+/*
+template <typename T>
+void BSP_Tree<T>::BuildTree(BSP_Tree* _tree, NodeVec _list, SDL_Rect* _partition, int branchLevel) {
+	// Set partition
+	_tree->partition = _partition;
+
+	// Add list to current tree
+	_tree->AddItems(_list);
+
+	// If no sub-branches, return.
+	if (branchLevel == 0)
+		return;
+
+	SDL_Rect* frontRect;
+	SDL_Rect* backRect;
+	NodeVec frontList;
+	NodeVec backList;
+
+
+	// Set front/back rects
+	if (_tree->partition->w > _tree->partition->h) {
+		Uint32 halfWidth = _tree->partition->w / 2;
+		frontRect = Graphics::CreateRect(halfWidth, _tree->partition->h, _tree->partition->x, _tree->partition->y);
+		backRect = Graphics::CreateRect(halfWidth, _tree->partition->h, _tree->partition->x + halfWidth, _tree->partition->y);
+	}
+	else {
+		Uint32 halfHeight = _tree->partition->h / 2;
+		frontRect = Graphics::CreateRect(_tree->partition->w, halfHeight, _tree->partition->x, _tree->partition->y);
+		backRect = Graphics::CreateRect(_tree->partition->w, halfHeight, _tree->partition->x, _tree->partition->y + halfHeight);
+	}
+
+
+	// Sort items into partition lists
+	for (T* item : _list) {
+		if (SDL_HasIntersection(item->Rect(), frontRect))
+			frontList.push_back(item);
+		if (SDL_HasIntersection(item->Rect(), backRect))
+			backList.push_back(item);
+	}
+
+	// If a list has items, create new sub-branch.
+	if (!frontList.empty()) {
+		_tree->front = new BSP_Tree();
+		BSP_Tree::BuildTree(_tree->front, frontList, frontRect, branchLevel - 1);
+	}
+	if (!backList.empty()) {
+		_tree->back = new BSP_Tree();
+		BSP_Tree::BuildTree(_tree->back, backList, backRect, branchLevel - 1);
+	}
+}
+*/
